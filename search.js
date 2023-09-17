6dd476ff9405e8aa8f9f323ec3b6974dbc1d67de
Abir Neogi <142147814+Urcher-226@users.noreply.github.com>
2023-09-18 01:06:58 +0530

    added

---
+++ search.js
@@ -0,0 +1,36 @@
+function performSearch() {
+    const query = document.getElementById("searchInput").value;
+    if (query=="dororo" || query=="DORORO" || query=="Dororo") {
+        open('dororo1.html');
+    }
+    else if(query=="spyxfamily" || query=="SPYXFAMILY" || query=="spyXfamily" || query=="SPYxFAMILY"){
+        open('sosa1.html');
+    }
+    else if(query=="bluelock" || query=="BLUELOCK" || query=="BLUE LOCK" || query=="blue lock" || query=="Blue Lock"){
+        open('mama1.html');
+    }
+    else if(query=="BLEACH" || query=="bleach" || query=="Bleach"){
+        open('sosa1.html');
+    }
+    else if(query=="onepiece" || query=="ONEPIECE" || query=="ONE PIECE" || query=="one piece" || query=="One Piece" || query=="One piece"){
+        open('mama1.html');
+    }
+    else if(query=="naruto" || query=="NARUTO"){
+        open('mamu1.html');
+    }
+    else if(query=="yourname" || query=="YOURNAME" || query=="YOUR NAME" || query=="your name" || query=="name" || query=="NAME" || query=="Your Name" || query=="Name" || query=="Your name"){
+        open('name.html');
+    }
+    else if(query=="suzumenotojimari" || query=="SUZUMENOTOJIMARI" || query=="SUZUME NO TOJIMARI" || query=="suzume no tojimari" || query=="suzume" || query=="SUZUME" || query=="Suzume" || query=="Suzume no tojimari"){
+        open('suzume.html');
+    }
+    else if(query=="dragonballz" || query=="DRAGONBALLZ" || query=="DRAGON BALL Z" || query=="DRAGON BALL z"  || query=="dragon ball Z" || query=="dragon ball z" || query=="dragon ball" || query=="DRAGON BALL" || query=="DRAGONBALL" || query=="dragonball" || query=="Dragon Ball Z" || query=="Dragon Ball" || query=="Dragon ball" || query=="Dragon ball z"){
+        open('goku.html');
+    }
+    else if(query=="demonslayer" || query=="DEMONSLAYER" || query=="DEMON SLAYER" || query=="demon slayer" || query=="Demon Slayer" || query=="Demon slayer"){
+        open('tanji1.html');
+    }
+    else if(query=="hellsparadise" || query=="HELLSPARADISE" || query=="HELLS PARADISE" || query=="hells paradise" || query=="Hells Paradise" || query=="Hells paradise"){
+        open('hell.html');
+    }
+}
