89de0c6104097a62c55d586fc4b32e761cb84daf
Abir Neogi <142147814+Urcher-226@users.noreply.github.com>
2023-09-18 00:13:15 +0530

    added

---
+++ pasword.js
@@ -0,0 +1,12 @@
+function performlogin() {
+    const boy = document.getElementById("searchInput").value;
+    const bad = document.getElementById("search").value;
+    var a="kali";
+    var b="kali";
+    if ((boy==a) && (bad==b)) {
+        open('index1.html');
+    }
+    else{
+        console.log('Stop looking in private site...');
+    }
+}
